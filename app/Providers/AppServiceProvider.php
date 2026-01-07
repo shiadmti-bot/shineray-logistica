35126768913c308a3aa68221661393b25b236f2a
DelcioFDNeto <delsinho2005@gmail.com>
2026-01-07 14:44:54 -0300

    Fix: Forcando HTTPS e corrigindo rota do Manifest

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <--- Importante: Adicione isso
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Vite::prefetch(concurrency: 3);
+        // SEGREDO DO DEPLOY NA VERCEL:
+        // Força HTTPS se estiver em produção, resolvendo o "Mixed Content"
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,6 +12,7 @@
         <link rel="manifest" href="/manifest.json">
         <meta name="theme-color" content="#DC2626">
         <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
