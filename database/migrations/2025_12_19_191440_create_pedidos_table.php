57afb648759d366bad7cc48c04dfda28a25b2040
DelcioFDNeto <shiadmti@gmail.com>
2026-01-13 15:17:29 -0300

    migrate atualizado

--- database/migrations/2025_12_19_191440_create_pedidos_table.php
+++ database/migrations/2025_12_19_191440_create_pedidos_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     Schema::create('pedidos', function (Blueprint $table) {
         $table->id();
         $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Quem pediu
-        $table->enum('status', ['em_analise', 'solicitado', 'separado', 'em_transito', 'concluido', 'cancelado'])->default('em_analise');
+        $table->enum('status', ['em_analise', 'solicitado', 'separado', 'expedido', 'em_transito', 'concluido', 'cancelado'])->default('em_analise');
         $table->text('observacao')->nullable();
         $table->timestamps();
     });
